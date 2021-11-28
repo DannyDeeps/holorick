4f5acc6e4ec5f7d57347055541e757f9474e8841
Daniel Huggins <contactdanielhuggins@gmail.com>
2021-11-28 18:19:54 +0000

    initial commit follow-up

--- holorick.php
+++ holorick.php
@@ -4,16 +4,15 @@ require_once __DIR__ . '/inc/start.php';
 
 use Discord\Discord;
 use Discord\WebSockets\Event;
-use Discord\Parts\{ Channel\Message, User\Member, WebSockets\MessageReaction };
-use Discord\Parts\Embed\{ Embed, Field };
-use React\EventLoop\Factory;
+use Discord\Parts\Channel\Message;
+use React\EventLoop\Loop;
 
-use Holorick\Identify\{ Roles, Channels };
+use Holorick\Identify\Channels;
 use Holorick\Cred\Cred;
 
 $discord= new Discord([
     'token' => Cred::D_TOKEN,
-    'loop' => Factory::create(),
+    'loop' => Loop::get(),
     'disabledEvents' => [],
     'loadAllMembers' => true
 ]);
@@ -23,7 +22,6 @@ $discord->on('ready', function(Discord $discord)
     // Message Router
     $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord)
     {
-        // Do not trigger on bot messages.
         if ($message->author->user->bot) return;
 
         switch ($message->channel_id)
@@ -42,6 +40,5 @@ function validateScreenshotMessage(Message $message)
     if (empty($message->attachments)) {
         $message->delete();
         $message->reply('Hey.. th.. _blugggh_.. that doesn\'t go there!');
-        echo print_r($message, true);
     }
 }
--- src/Identify/Channels.php
+++ src/Identify/Channels.php
@@ -4,6 +4,6 @@ namespace Holorick\Identify;
 
 class Channels
 {
-    const MOE_SCREENSHOTS= '825144851267977256';
+    const MOE_SCREENSHOTS= '914483890217951232';
     const COMMANDS= '735531477361623070';
 }
