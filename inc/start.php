c2dfdd5a0ded9b57061cf7ea7b697f2c0d21fad0
Daniel Huggins <contactdanielhuggins@gmail.com>
2024-01-30 13:44:01 +0000

    anonymise discord token

--- inc/start.php
+++ inc/start.php
@@ -5,4 +5,4 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 const ROOT = __DIR__ . '/../';
-const D_TOKEN = 'ODI4MzUzNjkyMTIxMzAxMDM0.GQUMCq.dF2kMZCFIF9cNEwjHJqGKeZCEpcVGoNPk-_1Ko';
+const D_TOKEN = 'YOUR_DISCORD_TOKEN';
